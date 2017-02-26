690cce49e127e8a11405548cc631b32ac2e1fc5a
Michael Tarnow <miketar10@gmail.com>
2017-02-25 22:31:59 -0500

    Roster View Fixed

--- app/src/main/java/com/example/miket/inclassassignment05_michaelt/PlayerActivity.java
+++ app/src/main/java/com/example/miket/inclassassignment05_michaelt/PlayerActivity.java
@@ -3,6 +3,7 @@ package com.example.miket.inclassassignment05_michaelt;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,7 +17,7 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
-        getSupportActionBar().setTitle("Plahyer Roster");
+        getSupportActionBar().setTitle("Player Roster");
 
         Intent intent = getIntent();
         ArrayList<Player> players = (ArrayList<Player>) intent.getSerializableExtra("Player List");
@@ -26,8 +27,10 @@ public class PlayerActivity extends AppCompatActivity {
 
         for (int i = 0; players.size()> i; i++) {
             String s= players.get(i).toString();
-            displayMessage= displayMessage + "\n\n";
+            displayMessage= displayMessage + s + "\n\n";
         }
 
+        TextView rosterView= (TextView) findViewById(R.id.roster_textbox);
+        rosterView.setText("Roster: " + displayMessage);
     }
 }
